795b82f742396d45363eac86047fe1b5603dadbf
annie2527 <you@example.com>
2020-10-02 13:35:00 +0200

    third

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,13 @@
     
 // })
 // console.log('it works!');
-const pacMan = document.querySelector('.entity');
-let xpos = 0;
-let ypos = 0;
-pacMan.style.left = xpos + "px";
-pacMan.style.top = ypos + "px";
-
-
+// pacMan.style.left = xpos + "px";
+// pacMan.style.top = ypos + "px";
 
 class Pacman {
-    constructor(initialXpos, mouthOpen) {
+    constructor(initialXpos, initialYpos, mouthOpen) {
         this.xpos = initialXpos;
+        this.ypos = initialYpos
         this.mouth = mouthOpen;
     }
     moveRight() {
@@ -23,16 +19,17 @@ class Pacman {
         this.xpos -= 85
     }
     moveUp() {
-        this.ypos += 85
+        this.ypos -= 85
     }
     moveDown() {
-        this.ypos -= 85
+        this.ypos += 85
     }
     update() {
         this.pacMan.style.left = this.xpos + "px"
         this.pacMan.style.top = this.ypos + "px"
     }
     render() {
+        const pacMan = document.querySelector('.entity');
         document.addEventListener('keydown', (event) => {
             if(event.code === 'ArrowLeft') {
                 this.moveLeft()
@@ -41,18 +38,29 @@ class Pacman {
                 this.moveRight()
                 pacMan.style.backgroundPositionY = "340px"
             } else if(event.code === 'ArrowUp') {
-                
+                this.moveUp()
                 pacMan.style.backgroundPositionY = "85px"
             } else if(event.code === 'ArrowDown') {
-                move
+                this.moveDown()
                 pacMan.style.backgroundPositionY = "170px"
             }
-            pacMan.style.left = xpos + "px";
-            pacMan.style.top = ypos + "px";
+
+            pacMan.style.left = this.xpos + "px";
+            pacMan.style.top = this.ypos + "px";
+            
+            return document
           });
     }
+
+    mount() {
+        this.game = this.render()
+        this.update()
+    }    
 }
 
+const app = document.querySelector('#app')
+const pacpac = new Pacman(50, 50, true)
+pacpac.mount(app)
 // document.addEventListener('keydown', (event) => {
 //     if(event.code === 'ArrowLeft') {
 //         pacMan.style.backgroundPositionY = "255px";
